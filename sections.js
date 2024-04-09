16cdd9a4221207aff3879042a78f284899d217d4
Frank Hudson <117737645+Frank-Hudson@users.noreply.github.com>
2024-04-09 23:33:29 +0100

    Add World Building section in site

--- sections.js
+++ sections.js
@@ -2,11 +2,11 @@ import { HAGUXINE_TO_ENGLISH, Word } from "./words.js";
 
 // const h = (level, content, id = "") =>
 //     `<h${level} ${id ? `id="${id}"` : ""}>${content}</h${level}>`;
-// const p = (content, id = "") => `<p ${id ? `id="${id}"` : ""}>${content}</p>`;
-// const a = (href, content, id = "") =>
+const p = (content, id = "") => `<p ${id ? `id="${id}"` : ""}>${content}</p>`;
+const a = (href, content, id = "") =>
 //     `<a ${id ? `id="${id}"` : ""} href="${href}">${content}</a>`;
 
-const webSection = function (title, content, level = 2) {
+const webSection = function(title, content, level = 2) {
     return `<div class="section">
                     <h${level} id="${title.split("<em")[0].replace('"', "").trim()}">${title}</h${level}>
                     <div>${content}</div>
@@ -19,6 +19,15 @@ outline.innerHTML += Array.from(HAGUXINE_TO_ENGLISH)
             `<li><a href="#${haguxine.word}">${haguxine.word}</a></li>`
     )
     .join("\n");
+
+content.innerHTML += webSection(
+    "World Building",
+    [
+        p("The speakers of this language primarily reside in non-snowy tundra and forest areas, overshadowed by large mountains and leading out into large plains and cold savannahs."),
+        p("Their main source of food is small-medium game animals, small poultry, and limited fruit and veg, primarily in the form of berries and roots."),
+        p(`They often keep either ${a("https://en.wiktionary.org/wiki/cervine", "cervine")}- or canine-like animals that live in the surrounding areas at pets. Occasionally, someone will take a bird as a pet, but usually nothing more flamboyant than that.`),
+    ].join("\n"),
+);
 
 content.innerHTML += webSection(
     "Words",
