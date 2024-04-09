0ed9bc5f72806d2f8b9622934e8a02f9bf02d891
Frank Hudson <117737645+Frank-Hudson@users.noreply.github.com>
2024-04-09 15:11:04 +0100

    Add map of haguxine to english words

--- words.js
+++ words.js
@@ -1,6 +1,20 @@
 class Word {
-    constructor(word, description) {
+    constructor(word, description = "Undescribed as of yet", pronunciation = "NONE") {
         this.word = word;
         this.description = description;
+        this.pronunciation = pronunciation;
     }
 }
+
+const HAGUXINE_TO_ENGLISH = new Map([
+    [new Word("kilik"), new Word("fire")],
+    [new Word("xun"), new Word("water")],
+    [new Word("vikha"), new Word("snow")],
+    [new Word("dokan"), new Word("wood")],
+    [new Word("ika"), new Word("sweet")],
+    [new Word("dokanika"), new Word("berry")],
+    [new Word("xkuwa"), new Word("juice")],
+    [new Word("xkudokna"), new Word("berry juice")],
+]);
+
+export { Word, HAGUXINE_TO_ENGLISH };
