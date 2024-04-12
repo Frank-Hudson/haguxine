e0db65f521b4aa494c5fa85a0557127c8f79f80b
Frank-Hudson <frank.hudson.v0@gmail.com>
2024-04-12 11:33:52 +0100

    YAGNI: description field #6

--- words.js
+++ words.js
@@ -1,20 +1,25 @@
 class Word {
-    constructor(word, description = "", pronunciation = "") {
+    constructor(word, pronunciation = "") {
         this.word = word;
-        this.description = description;
         this.pronunciation = pronunciation;
+        this.description = "";
+    }
+
+    withDescription(description) {
+        this.description = description;
+        return this;
     }
 }
 
 const HAGUXINE_TO_ENGLISH = new Map([
-    [new Word("kilik", "", "kiɭik"), new Word("fire", "", "")],
-    [new Word("xun", "", "ʃɯn"), new Word("water", "", "")],
-    [new Word("vikha", "", "viʜa"), new Word("snow", "", "")],
-    [new Word("dokan", "", "ɖɔkan"), new Word("wood", "", "")],
-    [new Word("ika", "", "ika"), new Word("sweet", "", "")],
-    [new Word("dokanika", "", "ɖɔkanika"), new Word("berry", "", "")],
-    [new Word("xkuwa", "", "ʃkɯwa"), new Word("juice", "", "")],
-    [new Word("xkudokna", "", "ʃkɯɖɔkna"), new Word("berry juice", "", "")],
+    [new Word("kilik",    "kiɭik"   ), new Word("fire"       )],
+    [new Word("xun",      "ʃɯn"     ), new Word("water"      )],
+    [new Word("vikha",    "viʜa"    ), new Word("snow"       )],
+    [new Word("dokan",    "ɖɔkan"   ), new Word("wood"       )],
+    [new Word("ika",      "ika"     ), new Word("sweet"      )],
+    [new Word("dokanika", "ɖɔkanika"), new Word("berry"      )],
+    [new Word("xkuwa",    "ʃkɯwa"   ), new Word("juice"      )],
+    [new Word("xkudokna", "ʃkɯɖɔkna"), new Word("berry juice")],
 ]);
 
 export { Word, HAGUXINE_TO_ENGLISH };
