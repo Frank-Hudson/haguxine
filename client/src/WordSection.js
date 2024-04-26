cc8810fc72b4fb9e3c55a572b708c69117007626
Frank-Hudson <frank.hudson.v0@gmail.com>
2024-04-26 17:52:10 +0100

    Format WordSection.js

--- client/src/WordSection.js
+++ client/src/WordSection.js
@@ -1,13 +1,14 @@
 export default function WordSection(properties) {
     return (
-        <div
-            className="word-section"
-            {...properties}
-        >
+        <div className="word-section" {...properties}>
             <h2>{properties.word}</h2>
             <p className="pronunciation">{`/${properties.pronunciation}/`}</p>
             <p className="translation">{properties.translation}</p>
-            <p className="definition">{properties.definition ? properties.definition : "No definition available"}</p>
+            <p className="definition">
+                {properties.definition
+                    ? properties.definition
+                    : "No definition available"}
+            </p>
         </div>
     );
 }
