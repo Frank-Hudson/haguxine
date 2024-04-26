f7a5361ce1664bb13c9eeb5109a1b91ab766c2c0
Frank-Hudson <frank.hudson.v0@gmail.com>
2024-04-26 18:53:45 +0100

    Minor updates to WordSection component

--- client/src/WordSection.js
+++ client/src/WordSection.js
@@ -1,6 +1,9 @@
 export default function WordSection(properties) {
+    let filteredProperties = Object.assign({}, properties);
+    delete filteredProperties.className;
+    delete filteredProperties.id;
     return (
-        <div className="word-section" {...properties}>
+        <div className={"word-section " + properties.className} id={properties.title} {...filteredProperties}>
             <h2>{properties.word}</h2>
             <p className="pronunciation">{`/${properties.pronunciation}/`}</p>
             <p className="translation">{properties.translation}</p>
