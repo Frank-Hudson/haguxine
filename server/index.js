d7064dc1239dae4688d8204012ada72255993972
Frank-Hudson <frank.hudson.v0@gmail.com>
2024-04-26 18:18:28 +0100

    Minor refactoring in server/index.js

--- server/index.js
+++ server/index.js
@@ -1,15 +1,19 @@
 const express = require("express");
 const fs = require("fs");
 
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
 
 const DATA = {
-    words: JSON.parse(fs.readFileSync(__dirname + "/data/words.json")),
+    words: JSON.parse(fs.readFileSync("server/data/words.json")),
 };
 
-app.get("/api", (request, response) => response.json(DATA));
-app.get("/api/words", (request, response) => response.json(DATA.words));
+app.get("/api", (request, response) => {
+    response.json(DATA);
+});
+app.get("/api/words", (request, response) => {
+    response.json(DATA.words);
+});
 
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT} (http://localhost:${PORT})`);
